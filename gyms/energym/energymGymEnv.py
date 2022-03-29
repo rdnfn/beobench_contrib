8d1b809b9863eeb8c2e0fcd095725d723bd74625
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-03-29 10:52:59 +0000

    feat: add support for discrete actions by wrapping them.
    This also includes some minor formatting changes.

--- gyms/energym/energymGymEnv.py
+++ gyms/energym/energymGymEnv.py
@@ -1,3 +1,5 @@
+"""Module with wrapper for Energym gym that makes it compatible with OpenAI gym."""
+
 import gym
 import numpy as np
 import energym
@@ -7,7 +9,7 @@ from re import match
 
 
 class EnergymGymEnv(gym.Env):
-    '''
+    """
     Energym environment that follows gym interface.
 
     Attributes
@@ -83,23 +85,24 @@ class EnergymGymEnv(gym.Env):
         Calculates reward given environment observation
     compute_done()
         Raises True if at end of episode
-    '''
+    """
 
-    def __init__(self,
-                 env,
-                 max_episode_length=35040,
-                 step_period=15,
-                 normalize=True,
-                 discretize=False,
-                 discrete_bins=30
-                 ):
+    def __init__(
+        self,
+        env,
+        max_episode_length=35040,
+        step_period=15,
+        normalize=True,
+        discretize=False,
+        discrete_bins=30,
+    ):
 
         super().__init__()
 
         if normalize and discretize:
             raise ValueError(
-                'Energym cannot normalise and discretize the state/action spaces. Please choose to normalize '
-                ' OR discretize, not both.'
+                "Energym cannot normalise and discretize the state/action spaces."
+                " Please choose to normalize OR discretize, not both."
             )
 
         self.env = env
@@ -113,8 +116,8 @@ class EnergymGymEnv(gym.Env):
         self.act_keys = [key for key in self.env.get_inputs_names()]
         self.obs_keys = [key for key in self.env.get_outputs_names()]
         self.n_act = len(self.act_keys)
-        self.temps = list(filter(lambda t: match('Z\d\d_T', t), self.obs_keys))
-        self.power = ['Fa_Pw_All']
+        self.temps = list(filter(lambda t: match("Z\d\d_T", t), self.obs_keys))
+        self.power = ["Fa_Pw_All"]
 
         self.cont_actions = []
         self.discrete_actions = []
@@ -125,15 +128,20 @@ class EnergymGymEnv(gym.Env):
 
         ### UPDATE ACTION SPACE ###
 
-        # create gym action space from energym action space and get lower and upper bounds
+        # create gym action space from energym action space and get lower and
+        # upper bounds
         action_space = gym.spaces.Dict()
         act_low = {}
         act_high = {}
         for key in self.act_keys:
             action = act_space[key]
             if isinstance(action, energym.spaces.box.Box):
-                action_space[key] = gym.spaces.Box(low=action.low[0], high=action.high[0],
-                                                   shape=action.shape, dtype=np.float32)
+                action_space[key] = gym.spaces.Box(
+                    low=action.low[0],
+                    high=action.high[0],
+                    shape=action.shape,
+                    dtype=np.float32,
+                )
                 self.cont_actions.append(key)
                 act_low[key] = action.low[0]
                 act_high[key] = action.high[0]
@@ -150,15 +158,18 @@ class EnergymGymEnv(gym.Env):
         # normalise action space if prompted by user
         if normalize:
             for key in self.cont_actions:
-                action_space[key] = gym.spaces.Box(low=-1, high=1,
-                                                   shape=action_space[key].shape, dtype=np.float32)
+                action_space[key] = gym.spaces.Box(
+                    low=-1, high=1, shape=action_space[key].shape, dtype=np.float32
+                )
 
         # discretize action spaces if prompted by user
         if discretize:
             # Obtain values of discretized action space
             val_bins_act = {}
             for key in self.act_keys:
-                val_bins_act[key] = np.linspace(self.act_low[key], self.act_high[key], self.n_bins + 1)
+                val_bins_act[key] = np.linspace(
+                    self.act_low[key], self.act_high[key], self.n_bins + 1
+                )
             self.val_bins_act = val_bins_act
 
             # Convert Box spaces to Discrete spaces
@@ -174,8 +185,9 @@ class EnergymGymEnv(gym.Env):
         for key in self.obs_keys:
             obs = obs_space[key]
             if isinstance(obs, energym.spaces.box.Box):
-                observation_space[key] = gym.spaces.Box(low=obs.low[0], high=obs.high[0],
-                                                        shape=obs.shape, dtype=np.float32)
+                observation_space[key] = gym.spaces.Box(
+                    low=obs.low[0], high=obs.high[0], shape=obs.shape, dtype=np.float32
+                )
                 self.cont_obs.append(key)
                 obs_low[key] = obs.low[0]
                 obs_high[key] = obs.high[0]
@@ -190,15 +202,18 @@ class EnergymGymEnv(gym.Env):
         # normalise obs space if prompted by user
         if normalize:
             for key in self.cont_obs:
-                observation_space[key] = gym.spaces.Box(low=-1, high=1,
-                                                        shape=observation_space[key].shape, dtype=np.float32)
+                observation_space[key] = gym.spaces.Box(
+                    low=-1, high=1, shape=observation_space[key].shape, dtype=np.float32
+                )
 
         # discretize obs spaces if prompted by user
         if discretize:
             # Obtain values of discretized obs space
             val_bins_obs = {}
             for key in self.obs_keys:
-                val_bins_obs[key] = np.linspace(self.obs_low[key], self.obs_high[key], self.n_bins + 1)
+                val_bins_obs[key] = np.linspace(
+                    self.obs_low[key], self.obs_high[key], self.n_bins + 1
+                )
             self.val_bins_obs = val_bins_obs
 
             # Convert Box spaces to Discrete spaces
@@ -279,7 +294,9 @@ class EnergymGymEnv(gym.Env):
             np_seed (list): List of seeds used in this env's random number generators.
         """
         if seed is not None and not (isinstance(seed, int) and 0 <= seed):
-            raise error.Error(f"Seed must be a non-negative integer or omitted, not {seed}")
+            raise ValueError(
+                f"Seed must be a non-negative integer or omitted, not {seed}"
+            )
 
         seed_seq = np.random.SeedSequence(seed)
         np_seed = seed_seq.entropy
@@ -300,15 +317,24 @@ class EnergymGymEnv(gym.Env):
         obs = {}
         if self.normalize:
             for key in self.cont_obs:
-                obs[key] = np.array(2 * (observation[key] - self.obs_low[key]) /
-                                    (self.obs_high[key] - self.obs_low[key]) - 1, dtype=np.float32).reshape(1, )
+                obs[key] = np.array(
+                    2
+                    * (observation[key] - self.obs_low[key])
+                    / (self.obs_high[key] - self.obs_low[key])
+                    - 1,
+                    dtype=np.float32,
+                ).reshape(
+                    1,
+                )
 
                 # clip observations into normalised bounds
                 obs[key] = np.clip(obs[key], a_min=-1, a_max=1)
 
         elif self.discretize:
             for key in self.cont_obs:
-                obs[key] = np.digitize(observation[key], self.val_bins_obs[key], dtype=np.int32)
+                obs[key] = np.digitize(
+                    observation[key], self.val_bins_obs[key]  # , dtype=np.int32
+                )
 
         # convert to ndarray
         # observation = np.array(list(observation.values()), dtype=np.float).reshape(len(observation.values()), )
@@ -317,35 +343,50 @@ class EnergymGymEnv(gym.Env):
 
     def action_converter(self, action: dict) -> dict:
         """
-        Takes numpy array actions and converts to dictionary compatible with energym. This transformation is compatible
-        with both normalized and discrete action spaces.
+        Takes numpy array actions and converts to dictionary compatible with energym.
+        This transformation is compatible with both normalized and discrete action
+        spaces.
 
         Args:
-            action (np.array): Array of actions computed by the agent to be implemented in this step
+            action (np.array): Array of actions computed by the agent to be implemented
+                in this step
 
         Returns:
             action (dict): Dict of actions in format recognisable to energym
 
         Notes:
-            energym expects values in action dict to be lists, hence the square brackets around each value in
-            action.
+            energym expects values in action dict to be lists, hence the square brackets
+            around each value in action.
         """
 
         action = deepcopy(action)
 
+        # Account for fact that energym wraps discrete action scalars in list,
+        # gym does not.
+        # See https://github.com/rdnfn/beobench/issues/39
+        for dis_action in self.discrete_actions:
+            action[dis_action] = [action[dis_action]]
+
         if self.normalize:
             # un-normalise values
             for key in self.cont_actions:
-                action[key] = ((action[key] + 1) / 2) * (self.act_high[key] - self.act_low[key]) \
-                                    + self.act_low[key]
+                action[key] = ((action[key] + 1) / 2) * (
+                    self.act_high[key] - self.act_low[key]
+                ) + self.act_low[key]
 
                 # clip actions into appropriate bounds
-                action[key] = [np.clip(action[key], a_min=self.act_low[key], a_max=self.act_high[key])]
+                action[key] = [
+                    np.clip(
+                        action[key], a_min=self.act_low[key], a_max=self.act_high[key]
+                    )
+                ]
 
         elif self.discretize:
             # un-discretize values
             for key in self.cont_actions:
-                action[key] = [self.val_bins_act[key][action[key]]]  # index bins vals given action selected
+                action[key] = [
+                    self.val_bins_act[key][action[key]]
+                ]  # index bins vals given action selected
 
         return action
 
@@ -378,8 +419,10 @@ class EnergymGymEnv(gym.Env):
             if (low_discomfort_temp <= temp) and (temp <= high_discomfort_temp):
                 pass
             else:
-                discomfort -= discomfort_penalty * min((low_discomfort_temp - temp) ** 2,
-                                                       (high_discomfort_temp - temp) ** 2)
+                discomfort -= discomfort_penalty * min(
+                    (low_discomfort_temp - temp) ** 2,
+                    (high_discomfort_temp - temp) ** 2,
+                )
 
         # energy term in reward
         energy = -(observation[self.power[0]] * (self.step_period / 60)) / 1000  # kWh
@@ -407,7 +450,8 @@ class EnergymGymEnv(gym.Env):
         convert the max_episode_length (timesteps) to seconds.
         """
 
-        done = observation['time'] >= self.start_time + \
-               (self.max_episode_length * self.step_period * 60)
+        done = observation["time"] >= self.start_time + (
+            self.max_episode_length * self.step_period * 60
+        )
 
         return done
